75c2f61915795e8392b149dec3b78db0b743a1de
Drumede <eitan.ula@gmail.com>
2025-02-11 14:11:20 +0200

    the base thing is working !!

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 import scratchattach as sa
 from dotenv import load_dotenv
 import os
@@ -9,8 +9,22 @@ session = sa.login(SCRATCHUSER, SCRATCHPASS)
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    user = session.connect_user("drumede")
-    name = user.name
-    return f"<p>{name}</p>"
+@app.route('/<path:sub_path>')
+def home(sub_path):
+    path_info = sub_path.split("/")
+    if path_info[0] != "projects":
+        return "bad link dickhead"
+    project = session.connect_project(int(path_info[1]))
+    titl = project.title
+    instructions = project.instructions
+    notes = project.notes
+    desc = f"**Instructions**\n{instructions}\n\n**Notes and Credits**\n{project.notes}"
+    id = project.id
+    image = project.thumbnail_url
+    return render_template(
+        "project.html",
+        title = titl,
+        description = desc,
+        id = id,
+        img = image
+    )
